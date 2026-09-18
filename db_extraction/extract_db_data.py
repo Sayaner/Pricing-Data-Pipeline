0ad44ef440bdc9ecb50fc2d522fb6f1a4af5f33c
Kacper Zdrojewski <zdrojewskikacper.53@gmail.com>
2026-09-18 14:12:49 +0200

    Added data cleaning and db-excel data merging

--- db_extraction/extract_db_data.py
+++ db_extraction/extract_db_data.py
@@ -5,44 +5,47 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.engine import URL
 
 
-load_dotenv()
+def fetch_db_data():
+    load_dotenv()
 
-connection_url = URL.create(
-    "mssql+pyodbc",
-    host=os.getenv("DB_SERVER"),
-    database=os.getenv("DB_NAME"),
-    query={
-        "driver": os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server"),
-        "trusted_connection": "yes",
-        "Encrypt": "yes",
-        "TrustServerCertificate": "yes",
-    },
-)
+    connection_url = URL.create(
+        "mssql+pyodbc",
+        host=os.getenv("DB_SERVER"),
+        database=os.getenv("DB_NAME"),
+        query={
+            "driver": os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server"),
+            "trusted_connection": "yes",
+            "Encrypt": "yes",
+            "TrustServerCertificate": "yes"
+        }
+    )
 
-engine = create_engine(connection_url)
+    engine = create_engine(connection_url)
 
-try:
-    with engine.connect() as connection:
-        db_df = pd.read_sql(
-            text("""SELECT
-            TW.Twr_Kod AS [Kod],
-            TW.Twr_KodDostawcy AS [Kod_Dostawcy],
-            K.Knt_Kod AS [Dostawca],
-            MAX(CASE WHEN TC.TwC_TwCNumer = 2 THEN TC.TwC_Wartosc END) AS [Cena_Cennikowa],
-            MAX(CASE WHEN TC.TwC_TwCNumer = 7 THEN TC.TwC_Wartosc END) AS [Cena_CZK],
-            MAX(CASE WHEN TC.TwC_TwCNumer = 8 THEN TC.TwC_Wartosc END) AS [Cena_USD],
-            MAX(CASE WHEN TC.TwC_TwCNumer = 9 THEN TC.TwC_Wartosc END) AS [Cena_PLN],
-            MAX(CASE WHEN TC.TwC_TwCNumer = 10 THEN TC.TwC_Wartosc END) AS [Cena_EUR]
-            FROM CDN.Towary TW
-            LEFT JOIN CDN.TwrCeny TC ON TW.Twr_TwrId = TC.TwC_TwrID
-            LEFT JOIN CDN.Kontrahenci K ON TW.Twr_KntId = K.Knt_KntId
-            WHERE K.Knt_Kod = 'SIOT'
-            GROUP BY TW.Twr_Kod, TW.Twr_KodDostawcy, K.Knt_Kod"""),
-            connection
-        )
-except Exception as e:
-    print(f"Błąd połączenia z bazą: {e}")
+    try:
+        with engine.connect() as connection:
+            db_df = pd.read_sql(
+                text("""SELECT
+                TW.Twr_Kod AS [Kod],
+                TW.Twr_KodDostawcy AS [Kod_Dostawcy],
+                K.Knt_Kod AS [Dostawca],
+                MAX(CASE WHEN TC.TwC_TwCNumer = 2 THEN TC.TwC_Wartosc END) AS [Cena_Cennikowa],
+                MAX(CASE WHEN TC.TwC_TwCNumer = 7 THEN TC.TwC_Wartosc END) AS [Cena_CZK],
+                MAX(CASE WHEN TC.TwC_TwCNumer = 8 THEN TC.TwC_Wartosc END) AS [Cena_USD],
+                MAX(CASE WHEN TC.TwC_TwCNumer = 9 THEN TC.TwC_Wartosc END) AS [Cena_PLN],
+                MAX(CASE WHEN TC.TwC_TwCNumer = 10 THEN TC.TwC_Wartosc END) AS [Cena_EUR]
+                FROM CDN.Towary TW
+                LEFT JOIN CDN.TwrCeny TC ON TW.Twr_TwrId = TC.TwC_TwrID
+                LEFT JOIN CDN.Kontrahenci K ON TW.Twr_KntId = K.Knt_KntId
+                WHERE K.Knt_Kod = 'SIOT' AND TW.Twr_KodDostawcy <> ''
+                GROUP BY TW.Twr_Kod, TW.Twr_KodDostawcy, K.Knt_Kod"""),
+                connection
+            )
 
+        db_df["Kod"] = db_df["Kod"].str.strip()
+        return db_df
 
-print(db_df)
+    except Exception as e:
+        print(f"Błąd połączenia z bazą: {e}")
+        return None
 
--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import pandas as pd
+from db_extraction.extract_db_data import fetch_db_data
+
+
+def clean_excel(excel_data):
+    excel_data = excel_data.drop(columns=["Cat. number", "Title", "Manufacturer", "Gross price"])
+    excel_data = excel_data.dropna(subset=["Code"])
+    excel_data = excel_data.fillna({"Net price": 0})
+    excel_data = excel_data.drop_duplicates(subset=["Code"])
+    excel_data["Code"] = excel_data["Code"].astype('str').str.strip()
+    excel_data["Net price"] = excel_data["Net price"].astype('float64')
+
+    return excel_data
 
 
 if __name__ == '__main__':
-    pass
-
+    db_df = fetch_db_data()
+    excel_df = pd.read_excel('pricing_files/cennik_siot.xlsx')
+    excel_df = clean_excel(excel_df)
+    result_df = db_df.merge(
+        excel_df,
+        left_on="Kod",
+        right_on="Code",
+        how="left"
+    )
+    print(result_df.info())
